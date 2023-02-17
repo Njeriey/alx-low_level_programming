74e43dac969966623add7ceaf4239dbaaa81561d
Yvonne <sherrymuriuki007@gmail.com>
2023-02-17 13:20:05 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single number of base 10 startin from 0, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+	        ;putchar(',');
+		;putchar(' ');
+		}
+		c++;
+	}
+}
